74c902bdd9cdd8e71c4f7d31cd16851271f32e55
SwapyDev <sabraham2342@gmail.com>
2024-08-24 16:10:56 -0600

    Implemented Merge Sort logic

--- MergeSort.py
+++ MergeSort.py
@@ -4,20 +4,47 @@ elements=int(input("Array length: "))
 def array(array, n):
     for i in range(n):
         if n>0:
-            element=input(f"Element {i+1}: ")
+            element=int(input(f"Input {i+1}: "))
             array.append(element)
         elif n-1==i:
             return array
 
 array(testArray,elements)
-print(testArray)
+print("Unordered",testArray)
 
 def mergeSort(array):
-    middle = len(array) // 2
-    leftArray = array[:middle]
-    rightArray = array[middle:]
-    print(leftArray, rightArray)
-    
+    if len(array) > 1:
+        mid = len(array) // 2  
+        left_half = array[:mid]  
+        right_half = array[mid:]
+        
+        mergeSort(left_half)
+        mergeSort(right_half)
+        
+        i=j=k=0
+        
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] > right_half[j]:
+                array[k] = left_half[i]
+                i += 1
+            else:
+                array[k] = right_half[j]
+                j += 1
+            k += 1
+
+        #Verifies if there are any elements left in the left half
+        while i < len(left_half):
+            array[k] = left_half[i]
+            i += 1
+            k += 1
+
+        #Verifies if there are any elements left in the right half
+        while j < len(right_half):
+            array[k] = right_half[j]
+            j += 1
+            k += 1
+            
 mergeSort(testArray)
+print("Ordered", testArray)
 
 
